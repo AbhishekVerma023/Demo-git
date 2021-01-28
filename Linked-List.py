af4854f6fec48f1162834ced6377b1f908328bf7
Abhishek Verma <abhishek.verma@freshgravity.com>
2021-01-28 16:01:39 +0530

    Created Node Class

---
+++ Linked-List.py
@@ -0,0 +1,17 @@
+class Node(object):
+    
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+
+    def setData(self, data):
+        self.data = data
+
+    def getData(self):
+        return self.data
+
+    def setNext(self, next):
+        self.next = next
+
+    def getNext(self):
+        return self.next
